30c70fab48dc57505049daf112027f0e7b044b35
BizPOS Admin <biztonicautomation@gmail.com>
2026-06-28 18:26:06 +0530

    fix: import AppTypography in pos_scaffold.dart to fix web compilation error

--- lib/core/design/layouts/pos_scaffold.dart
+++ lib/core/design/layouts/pos_scaffold.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:biztonic_pos/core/design/tokens/app_spacing.dart';
+import 'package:biztonic_pos/core/design/tokens/app_typography.dart';
 
 import 'package:provider/provider.dart';
 import 'package:go_router/go_router.dart';
